c2f958944449e50b899d1105ca492a0e2285a346
Chawin Aiemvaravutigul <nick41746@hotmail.com>
2022-02-11 01:09:03 +0700

    add bot

---
+++ bot_change_nickname.py
@@ -0,0 +1,32 @@
+import discord
+from discord.ext import commands
+import time
+
+bot = commands.Bot(command_prefix='>')
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send('pong')
+
+user_process = {}
+@bot.command(pass_context=True)
+async def samnick(ctx, member: discord.Member):
+    id = member._user.id
+    user_process.pop(id, None)
+
+@bot.command(pass_context=True)
+async def amnick(ctx, member: discord.Member, text):
+    id = member._user.id
+    if user_process.get(id, False):
+        return
+    user_process[id] = True
+    name = text
+    i = 0
+    while user_process.get(id, False):
+        new_name = name + '.' * (i+1)
+        await member.edit(nick=new_name)
+        i = (i + 1)%3
+        time.sleep(0.2)
+    # await ctx.send(f'Nickname was changed for {member.mention} ')
+
+bot.run('my token')
